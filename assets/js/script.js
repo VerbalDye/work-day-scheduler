30c475a1744a79c3ae18dedd6ed4e4baff1ac01f
VerbalDye <dhunts258@gmail.com>
2022-03-20 16:53:42 -0400

    commented up the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,20 @@
 var date = moment().format('dddd, MMMM Do');
 $("#currentDay").text(date);
 
+// object for all time values
 var taskSchedule = {};
 
+// create or populates the object from local storage
 var setupTask = function() {
+    // if check if the variable exist in local storage yet
     if (localStorage.getItem("taskSchedule")) {
+        // pulls from local then writes that to each of the text areas
         taskSchedule = JSON.parse(localStorage.getItem("taskSchedule"));
         for (var i = 8; i < 18; i++) {
             $("#" + i).val(taskSchedule[i]);
         }
     } else {
+        // create an empty object and then assigns that to local
         for (var i = 8; i < 18; i++) {
             taskSchedule[i] = "";
         }
@@ -18,10 +23,10 @@ var setupTask = function() {
     }
 }
 
+// saves current tasks to local
 var updateLocal = function() {
     localStorage.setItem("taskSchedule", JSON.stringify(taskSchedule));
 }
-
 
 // goes through each hour and colors it according to it's relation to the current time
 var colorTimes = function() {
@@ -48,11 +53,13 @@ var colorTimes = function() {
     });
 }
 
-$(".time-block").on("click", "button", function() {
+// listens for click on the save button
+$(".time-block").on("click", ".saveBtn", function() {
+    // get the value and id of the text area
     var textId = $(this).prev().attr("id");
     var textVal = $(this).prev().val().trim();
-    console.log(textId);
-    console.log(textVal);
+
+    // set the object with the new info then updates storage
     taskSchedule[textId] = textVal;
     updateLocal();
 });
